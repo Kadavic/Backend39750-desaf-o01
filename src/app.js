307ff08ef62b54088fac4574cd5a405e7c0f4d08
Kadavic <adriangezequiel@gmail.com>
2023-04-10 20:02:52 -0300

    PreEntregaProyectoFinal01

--- src/app.js
+++ src/app.js
@@ -1,33 +1,17 @@
-import express from "express";
-import { productManager } from "./productManager.js";
+import express, { urlencoded } from 'express'
+import routerProducts from './routes/products.router.js'
+import routerCarts from './routes/carts.router.js'
 
-const app = express();
+const app = express()
+
+const PORT = 8080
+app.use(express.json())
+app.use(urlencoded({extended:true}))
+app.use('/api/products', routerProducts)
+app.use('/api/carts', routerCarts)
 
 
-
-app.use(express.urlencoded({extended:true}));
-
-
-app.get('/products', (req,res)=>{
-  const {limit} = req.query
-  productManager.getProducts().then((products)=>{
-    if(products.length===0) return res.send('No items')
-    if(limit){
-      return res.send(products.slice(0,limit))
-    }
-    res.send(products)
-  }).catch(err => {
-    res.send(`Ha ocurrido un error ${err} al cargar la página`)
-  })
+const server = app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${server.address().port}`)
 })
-
-app.get('/products/:productId',(req,res)=>{
-  const id = req.params.productId
-  productManager.getProductById(id).then(product=>{
-    res.send(product)
-  }).catch(err => {
-    res.send(`Ha ocurrido un error ${err} al cargar la página`)
-  })
-})
-
-app.listen(8080, ()=>{console.log("Server listening at Port 8080")})
+server.on('error', error => console.log(`Error en el servidor ${error}`))
--- src/productManager.js
+++ src/productManager.js
@@ -7,88 +7,94 @@ class ProductManager {
     this.path = path;
   }
   async #checkDB(){
-    if(fs.existsSync(this.path)){
-      this.products = JSON.parse(await fs.promises.readFile(this.path))
-    }
+    this.products = JSON.parse(await fs.promises.readFile(this.path))
   }
   async #updateDB(){
-    await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products)) 
   }
-  async addProduct(title, description, price, thumbnail, code, stock){
+  async addProduct({title, description, code, price, status=true, stock, category, thumbnails=[]}){
     await this.#checkDB()
     const isInArray = this.products.some(product => product.code === code)
-    if(isInArray === false && title && description && price && thumbnail && stock){
-      this.products.push({
-        id: uuidv4(),
-        title: title,
-        description: description,
-        price: price,
-        thumbnail: thumbnail,
-        code: code,
-        stock: stock
-      });
-      this.#updateDB
-      return "Producto agregado"
-    }else{
-      return "Producto repetido o faltan características"
-    }
+    return new Promise((res,rej)=>{
+      if(isInArray === false){
+        this.products.push({
+          id: uuidv4(),
+          title: title,
+          description: description,
+          code: code,
+          price: price,
+          status: status,
+          stock: stock,
+          category: category,
+          thumbnails: thumbnails    
+        });
+        this.#updateDB().then(
+          res("Product added")
+        )
+      }else{
+        rej("Repeated product or items lacking")
+      }
+    })
+    
   }
   async getProducts(){
     await this.#checkDB()
-    return this.products
+    return new Promise((res,rej)=>{
+      if(this.products){
+        res(this.products)
+      }else{
+        rej("No products found")
+      }
+    })
   }
   async getProductById(id){
     await this.#checkDB()
     const productFound = this.products.find(product => product.id === id)
-    if (productFound){
-      return productFound
-    }else{
-      return "Product not found"
-    }
+    return new Promise((res,rej)=>{
+      if(productFound){
+        res(productFound)
+      }else{
+        rej("The product isn't in Products DB")
+      }
+    })
   }
-  async updateProduct(id, title, description, price, thumbnail, code, stock){
+  async updateProduct({id, title, description, code, price, status, stock, category, thumbnails}){
     await this.#checkDB()
     const indexFound = this.products.findIndex(product => product.id === id)
-    if(indexFound !== -1){
-      this.products[indexFound] = {
-        id: id,
-        title: title,
-        description: description,
-        price: price,
-        thumbnail: thumbnail,
-        code: code,
-        stock: stock
+    return new Promise((res,rej)=>{
+      if(indexFound !== -1){
+        this.products[indexFound] = {
+          id: id,
+          title: title,
+          description: description,
+          code: code,
+          price: price,
+          status: status,
+          stock: stock,
+          category: category,
+          thumbnails: thumbnails 
+        }
+        this.#updateDB().then(
+          res("Producto actualizado")  
+        )
+      }else{
+        rej("Product not found") 
       }
-      await this.#updateDB()
-      return "Producto actualizado"
-    }else{
-      return "Not found"
-    }
+    })
   }
   async deleteProduct(id){
     await this.#checkDB()
     const indexFound = this.products.findIndex(product => product.id === id)
-    if(indexFound !== -1){
-      this.products.splice(indexFound,indexFound+1)
-      await this.#updateDB()
-      return "Producto eliminado"
-    }else{
-      return "Not found"
-    }
+    return new Promise((res,rej)=>{
+      if(indexFound !== -1){
+        this.products.splice(indexFound,indexFound+1)
+        this.#updateDB().then(
+          res("Product deleted") 
+        )
+      }else{
+        rej("Product not found")
+      }
+    })
   }
 }
 export const productManager = new ProductManager("./src/products.json");
-console.log(productManager.addProduct("Malvón","Arbusto", 5, "Sin imagen1","abc1",40));
-console.log(productManager.addProduct("Monstera deliciosa","Arbusto", 5, "Sin imagen2","abc2",40));
-console.log(productManager.addProduct("Lavanda","Arbusto ", 5, "Sin imagen3","abc3",40));
-console.log(productManager.addProduct("Manzano","Arbol", 20, "Sin imagen3","abc4",15));
-console.log(productManager.addProduct("Naranjo","Arbol", 20, "Sin imagen3","abc5",15));
-console.log(productManager.addProduct("Limonero","Arbol", 20, "Sin imagen3","abc6",15));
-console.log(productManager.addProduct("Aylostera","Cactus", 5, "Sin imagen3","abc7",50));
-console.log(productManager.addProduct("Browningia","Cactus", 5, "Sin imagen3","abc8",50));
-console.log(productManager.addProduct("Cereus","Cactus", 5, "Sin imagen3","abc9",50));
-console.log(productManager.addProduct("Hylocereus","Cactus", 5, "Sin imagen3","abc10",50));
-
-
-
-
